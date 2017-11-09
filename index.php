07b87e12ab6265e5ec67494fbb281940c6dbbd84
Maartenzand <maartenzand300@gmail.com>
2017-11-09 12:30:46 +0100

    3

--- index.php
+++ index.php
@@ -7,8 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>TESTEN +!
-        </title>
+        <title>Test pagina</title>
     </head>
     <body>
         <?php
